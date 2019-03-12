65e168fa5ffe49e869402c6d7d218dc4e43a0bf7
sujianyi <1163365420@qq.com>
2019-03-12 17:58:08 +0800

    c++作业

---
+++ 苏健一。/main(2.16).cpp
@@ -0,0 +1,21 @@
+#ienclude<iostream>
+
+using namespace std;
+
+ int x,y,a,b,c,d;
+
+int main()
+{
+
+  cout<<"Enter two integers";
+  cin>>x>>y;
+  a=x+y;
+  b=x-y;
+  c=x*y;
+  d=x/y;
+
+  cout <<"the product is"<<a<<endl;
+  cout<<b<<endl;
+  cout<<c<<endl;
+  cout<<d<<endl;
+}
